f9e649cf79a32241585d4f401023ee8c29382172
reynave <felix.renaldi@gmail.com>
2023-07-12 01:27:52 +0700

    update

--- ng16/src/app/template/template-table/template-table.component.ts
+++ ng16/src/app/template/template-table/template-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { NgbOffcanvas, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+declare var $: any;
 
 @Component({
   selector: 'app-template-table',
@@ -8,9 +9,9 @@ import { NgbOffcanvas, NgbModal } from '@ng-bootstrap/ng-bootstrap';
   encapsulation: ViewEncapsulation.None,
 })
 export class TemplateTableComponent implements OnInit{
-
+  leftSide : boolean = true;
   panels = ['First', 'Second', 'Third'];
-
+  fields: any = [];
   items: any = [];
   constructor(
     private offcanvasService: NgbOffcanvas, 
@@ -24,7 +25,15 @@ export class TemplateTableComponent implements OnInit{
         name: "test",
       });
     }
+    for (let i = 0; i < 15; i++) {
+      this.fields.push({
+        id: i + 1,
+        name: "Name_"+(i*7),
+      });
+    }
     console.log(this.items);
+
+   
   }
 
   openCanvas(content: any) {
@@ -41,12 +50,33 @@ export class TemplateTableComponent implements OnInit{
 
   openModalFullscreen(content: any) {
     this.modalService.open(content, { fullscreen: true }).result.then(
-			(result) => {
-				 
+			(result) => { 
+       
 			},
 			(reason) => {
 			 
 			},
 		);
+
+    $(function () {
+      $(".sortable").sortable({
+        handle: ".handle",
+        placeholder: "ui-state-highlight",
+        update: function (event: any, ui: any) {
+          const order: any[] = [];
+          $(".sortable .handle").each((index: number, element: any) => {
+            const itemId = $(element).attr("id");
+            order.push(itemId);
+          });
+
+          console.log(order);
+
+        }
+      });
+    });
+  }
+
+  update(){
+    console.log("update");
   }
 }
