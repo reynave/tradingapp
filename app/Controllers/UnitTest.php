01bf8c0fe3ede2c6b50d266172a97b0286a2e397
reynave <felix.renaldi@gmail.com>
2023-09-19 01:50:42 +0700

    update

---
+++ app/Controllers/UnitTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Model;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class UnitTest extends BaseController
+{
+    public function index()
+    {
+        $data = array(
+            "error" => false,
+        );
+        return $this->response->setJSON($data);
+    }
+
+
+    public function faker()
+    {
+        $faker = \Faker\Factory::create();
+       
+        $winrate = [-1,-1,-1,2.4,3];
+
+        for ($i = 0; $i < 2; $i++) {
+            $this->db->table("journal_detail")->insert([
+                'journalId' => 'J23000065',
+                'f1' => '2023-' . $faker->date('m-d'),
+                'f2' => $faker->date('H:i'),
+                'f3' => rand(101, 104),
+                'f4' => rand(106, 109),
+                'f5' => rand(30, 50),
+                'f6' => $winrate[rand(0, 4)],
+                'f9' => '2023-' . $faker->date('m-d'),
+                'f10' => $faker->date('H:i'),
+            ]);
+        }
+        $data = array(
+            "error" => false,
+        );
+        return $this->response->setJSON($data);
+    }
+
+}
